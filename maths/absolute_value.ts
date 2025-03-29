6d9eea43a8db563e1bf3408c6fd83afee20547cf
Mohammad Abdullah-Al-Sajid Chowdhury <75419355+SajidAbdullah729@users.noreply.github.com>
2025-03-29 07:20:27 +0600

    Update absolute_value.ts

--- maths/absolute_value.ts
+++ maths/absolute_value.ts
@@ -1,13 +1,4 @@
-/**
- * @function absoluteValue
- * @description Calculate the absolute value of an input number.
- * @param {number} number - a numeric input value
- * @return {number} - Absolute number of input number
- * @see https://en.wikipedia.org/wiki/Absolute_value
- * @example absoluteValue(-10) = 10
- * @example absoluteValue(50) = 50
- * @example absoluteValue(0) = 0
- */
+
 
 export const absoluteValue = (number: number): number => {
   return number < 0 ? -number : number
